acc1c7c739e936f0ae6ef4aaffd59597b4c09803
DawonEllaKim <dawonella0411@gmail.com>
2021-10-22 17:57:18 +0900

    김다원: final draft

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -134,6 +134,65 @@ const Detail = (props) => {
               </div>
             ) : null}
           </Info>
+
+          <div>
+            <Like>
+              <div>
+                <svg width="24" height="24" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z"
+                  ></path>
+                </svg>
+              </div>
+              <p>100</p>
+              <div>
+                <svg width="24" height="24" viewBox="0 0 24 24" class="share">
+                  <path
+                    fill="currentColor"
+                    d="M5 7c2.761 0 5 2.239 5 5s-2.239 5-5 5-5-2.239-5-5 2.239-5 5-5zm11.122 12.065c-.073.301-.122.611-.122.935 0 2.209 1.791 4 4 4s4-1.791 4-4-1.791-4-4-4c-1.165 0-2.204.506-2.935 1.301l-5.488-2.927c-.23.636-.549 1.229-.943 1.764l5.488 2.927zm7.878-15.065c0-2.209-1.791-4-4-4s-4 1.791-4 4c0 .324.049.634.122.935l-5.488 2.927c.395.535.713 1.127.943 1.764l5.488-2.927c.731.795 1.77 1.301 2.935 1.301 2.209 0 4-1.791 4-4z"
+                  ></path>
+                </svg>
+              </div>
+            </Like>
+            <Box>
+              <BoxContent>{userId + " 님의 게시물 입니다."}</BoxContent>
+              <RiBookmarkFill
+                style={{
+                  width: "35px",
+                  height: "40px",
+                  color: " rgb(18, 184, 134)",
+                  position: "absolute",
+                  top: "-8",
+                  right: "20",
+                  backgroundColor: "transparent",
+                }}
+              />
+              <BoxFooter>
+                <BoxFooterLeft>
+                  <Select></Select>
+                  목록 보기
+                </BoxFooterLeft>
+                <BoxFooterRight>
+                  <div
+                    style={{
+                      backgroundColor: "transparent",
+                      paddingBottom: "90px",
+                    }}
+                  >
+                    1/1
+                  </div>
+                  <Forward>
+                    <IoIosArrowDropleftCircle />
+                  </Forward>
+                  <Backward>
+                    <IoIosArrowDroprightCircle />
+                  </Backward>
+                </BoxFooterRight>
+              </BoxFooter>
+            </Box>
+          </div>
+
           <Content>
             <div>
               <Viewer initialValue={content} height="1000px" />
@@ -263,7 +322,7 @@ const Content = styled.div``;
 const Like = styled.div`
   position: fixed;
   top: 112px;
-  left: 550px;
+  left: 450px;
   width: 4rem;
   background: rgb(248, 249, 250);
   border: 1px solid rgb(241, 243, 245);
@@ -299,7 +358,7 @@ const Box = styled.div`
 
   border-radius: 5px;
 
-  width: 688.007px;
+  width: 720.007px;
   height: 100.861px;
   margin: 40px 0;
   padding: 32px 24px 10px 24px;
